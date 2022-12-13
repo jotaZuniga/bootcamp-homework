1aa591d6bf83e348402d19e3f09119a3c3305c8e
Jose Zuniga <josemzr@gmail.com>
2022-12-13 13:32:24 -0600

    adding API (#4)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-import express, { Application, Request, Response } from "express"
+import express, { Application } from "express"
+import * as dotenv from 'dotenv'
 import bodyParser from "body-parser"
-const app: Application = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.get("/", (req: Request, res: Response) => {
-  res.send("TS App is Running")
-})
-const PORT = 3000;
+import { DBConnect } from './db';
+import { moviesRouter } from "./routes/routes";
+
+dotenv.config();
+
+const app: Application = express();
+const mongoConnect = new DBConnect(process.env.MONGODB_URI, process.env.DB_NAME);
+
+const dbInit = mongoConnect.init();
+
+dbInit.then(()=> console.log('JOSE... DB is up and running'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(moviesRouter);
+
+const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
-  console.log(`JOSE, your server is running on PORT ${PORT}`)
+  console.log(`JOSE ZUNIGA,  your server is running on PORT ${PORT}`)
 })
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,75 @@
 import express, { Request, Response } from 'express';
+import { MOVIES } from '../mocks/movies';
+import { Movie } from '../Models/Movie';
 
 const router = express.Router();
 
-router.get('/', [], (req: Request, res: Response) => {
-  return res.send('/GET');
+router.get('/all', async (req: Request, res: Response) => {
+  const filter = {};
+  const all = await Movie.find(filter);
+  res.setHeader('Content-Type', 'application/json');
+
+  if (all.length > 0) {
+    return res.json({method: req.method, movies: all});
+  } else {
+    return res.json({method: req.method, movies: []});
+  }
 });
 
-router.post('/api', (req: Request, res: Response) => {
-  return res.send('/POST');
+router.post('/populate/:decision', async (req: Request, res: Response, next) => {
+  try {
+    const action = req.params.decision;
+    res.setHeader('Content-Type', 'application/json');
+
+    if (action === 'yes') {
+      MOVIES.forEach(async movie => {
+        const schema = new Movie(movie);
+        await schema.save();
+      })
+      return res.json({method: req.method, message: 'All movies have been inserted...'});
+    } else {
+      return res.json({method: req.method, message: `Action unknown: ${action}`});
+    }
+  } catch (error) {
+    console.log(error.message);
+    if (error.name === 'ValidationError') {
+      res.sendStatus(404);
+      return;
+    }
+    next(error);    
+  }
+});
+
+router.get('/findMovie/:name', async (req: Request, res: Response, next) => {
+  const name = req.params.name;
+  res.setHeader('Content-Type', 'application/json');
+  console.log(name);
+  try {
+    const movie = await Movie.find({title: name});
+
+    if (!movie) {
+      res.json({method: req.method, message: 'All movies have been inserted...'});
+    }
+    res.json({method: req.method, movie});
+  } catch (error) {
+    console.log(error.message);
+    if (error.name === 'ValidationError') {
+      res.sendStatus(404);
+      return;
+    }
+    next(error);
+  }
+});
+
+router.post('/add/:name', (req: Request, res: Response) => {
+  const name = req.params.name;
+  res.setHeader('Content-Type', 'application/json');
+  return res.json({method: req.method, message: `Adding pokemon: ${name}`});
+});
+
+router.delete('/:id', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'application/json');
+  return res.json({method: req.method, message: `Delete movie: ${req.params.id}`});
 });
 
 export { router as moviesRouter }
